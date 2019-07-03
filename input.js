b82015922c325d481d3cb18eb0b5c77d09ffb518
girOly <girardob@gmail.com>
2019-07-03 20:56:14 +0000

    Completed Snake Game

--- input.js
+++ input.js
@@ -1,4 +1,6 @@
-const setupInput = function() {
+let connection;
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -7,6 +9,18 @@ const setupInput = function() {
   const handleUserInput = stdin.on("data", key => {
     if (key === "\u0003") {
       process.exit();
+    }
+    if (key === "w") {
+      connection.write("Move: up");
+    }
+    if (key === "a") {
+      connection.write("Move: left");
+    }
+    if (key === "s") {
+      connection.write("Move: down");
+    }
+    if (key === "d") {
+      connection.write("Move: right");
     }
   });
 
