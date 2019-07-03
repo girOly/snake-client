88b2619f896ac98d9f868c5017fcd84e4b378b77
girOly <girardob@gmail.com>
2019-07-03 20:26:31 +0000

    Server intergration with Input file

--- play.js
+++ play.js
@@ -1,20 +1,9 @@
 // play.js
 const { connect } = require("./client");
+const { setupInput } = require("./input")
 console.log("Connecting ...");
 connect();
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  const handleUserInput =
-  stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  });
-  return stdin;
-};
+
 
 setupInput();
