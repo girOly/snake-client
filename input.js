3d3738c2f57407fd5c166aac027d006044629c83
girOly <girardob@gmail.com>
2019-07-03 20:28:36 +0000

    Module Exported

--- input.js
+++ input.js
@@ -3,11 +3,14 @@ const setupInput = function() {
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  const handleUserInput =
-  stdin.on('data', (key) => {
-    if (key === '\u0003') {
+
+  const handleUserInput = stdin.on("data", key => {
+    if (key === "\u0003") {
       process.exit();
     }
   });
+
   return stdin;
 };
+
+module.exports = { setupInput };
